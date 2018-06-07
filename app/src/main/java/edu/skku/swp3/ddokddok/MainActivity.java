45f8e5752a9a9fc198a02681215e4be77c8dd1ff
배요한 <john@devhi.me>
2018-06-07 19:22:57 +0900

    artik login activity

--- app/src/main/java/edu/skku/swp3/ddokddok/MainActivity.java
+++ app/src/main/java/edu/skku/swp3/ddokddok/MainActivity.java
@@ -1,9 +1,9 @@
 package edu.skku.swp3.ddokddok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
