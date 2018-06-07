75bd536e3ad0a3ff536c5b6a6f568ff5b8e2920e
배요한 <john@devhi.me>
2018-06-07 15:59:56 +0900

    Initial commit

---
+++ app/src/test/java/edu/skku/swp3/ddok_ddok/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.skku.swp3.ddok_ddok;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
